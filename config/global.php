02ad59765c3b20c9b8415f1e87ffafe9aa8ee3b4
CRjulianTICO <CRjulianTICO@gmail.com>
2018-06-27 23:03:09 -0600

    PrimerAvance

---
+++ config/global.php
@@ -0,0 +1,9 @@
+<?php 
+
+define("DB_HOST","localhost");
+define("DB_NAME", "SEDRI");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_ENCODE","utf8");
+define("PRO_NOMBRE","SEDRI");
+?>
