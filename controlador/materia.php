ff5ff75f7879597ac25bf2efc85dbe34a19d2ccc
yalmer <39608052+yalmer@users.noreply.github.com>
2018-08-04 19:35:51 -0700

    Update materia.php

--- controlador/materia.php
+++ controlador/materia.php
@@ -2,4 +2,8 @@
 require_once "../modelo/Materia.php";
 
 $instaMateria = new Materia();
+
+$id= isset($_POST["idmateria"])?limpiarCadena($_POST["idmateria"]):"";
+$nombre = isset($_POST["nombreMateria"])?limpiarCadena($_POST["nombreMateria"]):"";
+
 ?>
