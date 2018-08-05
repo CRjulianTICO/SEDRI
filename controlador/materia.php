7ce0eb22ce06255dad6883d694fe4645ff8d3512
yalmer <39608052+yalmer@users.noreply.github.com>
2018-08-04 19:48:27 -0700

    Update materia.php
    Controlador del mantenimiento de materias

--- controlador/materia.php
+++ controlador/materia.php
@@ -1,4 +1,4 @@
-?php 
+<?php 
 require_once "../modelo/Materia.php";
 
 $instaMateria = new Materia();
@@ -6,57 +6,60 @@ $instaMateria = new Materia();
 $id= isset($_POST["idmateria"])?limpiarCadena($_POST["idmateria"]):"";
 $nombre = isset($_POST["nombreMateria"])?limpiarCadena($_POST["nombreMateria"]):"";
 
+
 switch ($_GET["opcion"]){
 	case 'guardar':
 			$rspta=$instaMateria->insertar($nombre);
 			echo $rspta ? "Registrado" : "Error";
 	break;
 
-case 'editar':
+	case 'editar':
 			$rspta=$instaMateria->actualizar($id,$nombre);
 			echo $rspta ? "Registrado" : "Error".$id."/".$nombre;
 	break;
 
-case 'listar':
+	case 'listar':
 		$rspta=$instaMateria->listar();
  		$data= Array();
-                while ($reg=$rspta->fetch_object()){
+ 		while ($reg=$rspta->fetch_object()){
  			$data[]=array(
-
-                                      "0"=>$reg->idmateria,
+ 				
+ 				"0"=>$reg->idmateria,
  				"1"=>$reg->nombre,
  				"2"=>($reg->estado)?'<span class="label bg-green">Activado</span>':
  				'<span class="label bg-red">Desactivado</span>',
-                                "3"=>($reg->estado)?'<button class="mostrarEditar" onclick="mostrar('.$reg->idmateria.')"><i class="material-icons center blue-text ">edit</i></button>'.
+ 				"3"=>($reg->estado)?'<button class="mostrarEditar" onclick="mostrar('.$reg->idmateria.')"><i class="material-icons center blue-text ">edit</i></button>'.
  					' <button class="mostrarBlock" onclick="desactivar('.$reg->idmateria.')"><i class="material-icons center red-text ">block</i></button>':
  					'<button class="mostrarEditar" onclick="mostrar('.$reg->idmateria.')"><i class="material-icons center blue-text ">edit</i></button>'.
  					' <button class="mostrarCheck" onclick="activar('.$reg->idmateria.')"><i class="material-icons center green-text">check</i></button>',
  				);
-                }
+ 		}
  		$results = array(
  			"sEcho"=>1, //Información para el datatables
-                        "iTotalRecords"=>count($data), //enviamos el total registros al datatable
+ 			"iTotalRecords"=>count($data), //enviamos el total registros al datatable
  			"iTotalDisplayRecords"=>count($data), //enviamos el total registros a visualizar
  			"aaData"=>$data);
  		echo json_encode($results);
 	break;
-         
-        case 'desactivar':
+
+	case 'desactivar':
 		 $rspta = $instaMateria->desactivar($id);
 		 echo "string".$cedula;
 		 echo $rspta ? "Materia activada" : "Materia no se puede activar";
 		break;
 
-       case 'activar':
+	case 'activar':
 		 $rspta = $$instaMateria->activar($id);
 		 echo $rspta ? "Materia activada" : "Materia no se puede activar";
 		break;
 
-case 'mostrar':
+	case 'mostrar':
 		$rspta=$instaMateria->consultaEspecifica($id);
  		
  		echo json_encode($rspta);
 		break;
 
 }
+
+
 ?>
