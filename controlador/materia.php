d7b6b3a105d6e67508f5d4bcb837551d319d29ef
yalmer <39608052+yalmer@users.noreply.github.com>
2018-08-04 19:39:53 -0700

    Update materia.php

--- controlador/materia.php
+++ controlador/materia.php
@@ -6,4 +6,15 @@ $instaMateria = new Materia();
 $id= isset($_POST["idmateria"])?limpiarCadena($_POST["idmateria"]):"";
 $nombre = isset($_POST["nombreMateria"])?limpiarCadena($_POST["nombreMateria"]):"";
 
+switch ($_GET["opcion"]){
+	case 'guardar':
+			$rspta=$instaMateria->insertar($nombre);
+			echo $rspta ? "Registrado" : "Error";
+	break;
+
+case 'editar':
+			$rspta=$instaMateria->actualizar($id,$nombre);
+			echo $rspta ? "Registrado" : "Error".$id."/".$nombre;
+	break;
+
 ?>
