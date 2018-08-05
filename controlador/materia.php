04324aa2d131394180b82074297fe295647daa84
yalmer <39608052+yalmer@users.noreply.github.com>
2018-08-04 19:43:55 -0700

    Update materia.php

--- controlador/materia.php
+++ controlador/materia.php
@@ -35,4 +35,10 @@ case 'listar':
                 }
  		$results = array(
  			"sEcho"=>1, //Información para el datatables
+                        "iTotalRecords"=>count($data), //enviamos el total registros al datatable
+ 			"iTotalDisplayRecords"=>count($data), //enviamos el total registros a visualizar
+ 			"aaData"=>$data);
+ 		echo json_encode($results);
+	break;
+
 ?>
