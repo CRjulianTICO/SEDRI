fcb693d027784e9cae8a38c85cefa5829e78ebff
KendallGC <39597792+KendallGC@users.noreply.github.com>
2018-08-11 15:15:52 -0600

    Creación De Tablas
    alumno, alumno_encargado, alumno_materia, asistencia, beca, director, empleado, encargado, grado

--- BD Sedri.sql
+++ BD Sedri.sql
@@ -82,3 +82,137 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_InsertarProfesor` (IN `CED` VARC
 END$$
 
 DELIMITER ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `idalumno` int(11) NOT NULL AUTO_INCREMENT,
+  `Persona_idPersona` int(11) NOT NULL,
+  PRIMARY KEY (`idalumno`,`Persona_idPersona`),
+  UNIQUE KEY `idalumno_UNIQUE` (`idalumno`),
+  KEY `fk_alumno_Persona1_idx` (`Persona_idPersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno_encargado`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno_encargado` (
+  `ID_ALUMNO` int(11) NOT NULL,
+  `ID_ENCARGADO` int(11) NOT NULL,
+  PRIMARY KEY (`ID_ALUMNO`,`ID_ENCARGADO`),
+  KEY `ID_ENCARGADO` (`ID_ENCARGADO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno_materia`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno_materia` (
+  `alumno_idalumno` int(11) NOT NULL,
+  `materia_idmateria` int(11) NOT NULL,
+  `trimestre_1` decimal(10,0) DEFAULT NULL,
+  `trimestre_2` decimal(10,0) DEFAULT NULL,
+  `trimestre_3` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`alumno_idalumno`,`materia_idmateria`),
+  KEY `fk_alumno_has_materia_materia1_idx` (`materia_idmateria`),
+  KEY `fk_alumno_has_materia_alumno1_idx` (`alumno_idalumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistencia`
+--
+
+CREATE TABLE IF NOT EXISTS `asistencia` (
+  `IDASISTENCIA` int(11) NOT NULL AUTO_INCREMENT,
+  `ESTADO` tinyint(1) DEFAULT NULL,
+  `NOTA` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `IDALUMNO` int(11) DEFAULT NULL,
+  `FECHA` date DEFAULT NULL,
+  PRIMARY KEY (`IDASISTENCIA`),
+  KEY `FK_ASISTENCIA_ALUMNO` (`IDALUMNO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `beca`
+--
+
+CREATE TABLE IF NOT EXISTS `beca` (
+  `idbeca` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion_beca` varchar(45) NOT NULL,
+  `monto_beca` varchar(45) NOT NULL,
+  `idAlumno` int(11) NOT NULL,
+  `estado` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`idbeca`),
+  KEY `fk_beca_alumno_idx` (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `director`
+--
+
+CREATE TABLE IF NOT EXISTS `director` (
+  `idDirector` int(11) NOT NULL AUTO_INCREMENT,
+  `Persona_idPersona` int(11) NOT NULL,
+  PRIMARY KEY (`idDirector`,`Persona_idPersona`),
+  UNIQUE KEY `iddirector_UNIQUE` (`idDirector`),
+  KEY `fk_director_Persona1_idx` (`Persona_idPersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE IF NOT EXISTS `empleado` (
+  `idEmpleado` int(11) NOT NULL AUTO_INCREMENT,
+  `idPersona` int(11) NOT NULL,
+  `idPuesto` int(11) NOT NULL,
+  PRIMARY KEY (`idEmpleado`),
+  KEY `FK_EMPLEADO_PUESTO` (`idPuesto`),
+  KEY `FK_EMPLEADO_PERSONA` (`idPersona`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encargado`
+--
+
+CREATE TABLE IF NOT EXISTS `encargado` (
+  `idencargado` int(11) NOT NULL AUTO_INCREMENT,
+  `Persona_idPersona` int(11) NOT NULL,
+  PRIMARY KEY (`idencargado`,`Persona_idPersona`),
+  UNIQUE KEY `idencargado_UNIQUE` (`idencargado`),
+  KEY `fk_encargado_Persona1_idx` (`Persona_idPersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grado`
+--
+
+CREATE TABLE IF NOT EXISTS `grado` (
+  `idgrado` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreGrado` varchar(45) NOT NULL,
+  PRIMARY KEY (`idgrado`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
