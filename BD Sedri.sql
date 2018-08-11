78f3155edfccf2d21031804d5ead1f05ff3c02b5
KendallGC <39597792+KendallGC@users.noreply.github.com>
2018-08-11 15:00:00 -0600

    Script Creación BD

--- BD Sedri.sql
+++ BD Sedri.sql
@@ -1,1 +1,13 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-08-2018 a las 22:51:44
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
