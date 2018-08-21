1d3134bb8e8ba2f116f7d562ea656742fd057c31
OscarSL797 <oscarsoto040797@gmail.com>
2018-08-21 12:04:38 -0600

    Backend de Lista_Alumnos para director

--- vistas/UIListaAlumno.php
+++ vistas/UIListaAlumno.php
@@ -3,17 +3,16 @@
 <div id="content">
 
 <div id="tabla">
-    <table id="tblEmpleado" class="display responsive nowrap tabla" style="width: 100%">
+    <table id="tbAlumno" class="display responsive nowrap tabla" style="width: 100%">
         <thead>
           <th data-priority="1">Cedula</th>
           <th data-priority="2">Nombre</th>
-          <th data-priority="3">Primer Apellido</th>
+          <th data-priority="4">Primer Apellido</th>
           <th data-priority="5">Segundo Apellido</th>
           <th data-priority="6">Sexo</th>
           <th data-priority="7">Nacionalidad</th>
-          <th data-priority="8">Grado</th>
-          <th data-priority="4">Direccion</th>
-          <th data-priority="10">Opciones</th>
+          <th data-priority="3">Grado</th>
+          <th data-priority="8">Direccion</th>
         </thead>
         <tbody>
         </tbody>
