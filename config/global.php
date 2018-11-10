f7dd6d2dcef8572d75d495e8e9e3238d8ee0922b
Julian P.F <39641021+CRjulianTICO@users.noreply.github.com>
2018-11-09 23:39:16 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST","localhost");
+define("DB_HOST","35.229.51.55");
 define("DB_NAME", "escuela");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_USERNAME", "sedri");
+define("DB_PASSWORD", "123");
 define("DB_ENCODE","utf8");
 define("PRO_NOMBRE","SEDRI");
 ?>
